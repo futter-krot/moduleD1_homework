b08416679522ed0d535c917443beff3ccaef9c42
futter-krot <66520330+futter-krot@users.noreply.github.com>
2021-02-26 20:59:34 +0300

    Update trello.py

--- peer2peer/trello.py
+++ peer2peer/trello.py
@@ -9,63 +9,64 @@ auth_params = {
 base_url = "https://api.trello.com/1/{}" 
 board_id = "{}".format(login[2]) # ГЕНЕРИРУЕМ BOARD_ID НА ЛОГИНЕ
 def read():
-	column_data = requests.get(base_url.format('boards') + '/' + board_id + '/lists', params=auth_params).json()
-	tasklist = [] # СПИСОК, В КОТОРЫЙ БУДЕМ КЛАСТЬ ID ТАСКОВ
-	for column in column_data:
-		tasklist.append(task["id"])
-		task_data = requests.get(base_url.format('lists') + '/' + column['id'] + '/cards', params=auth_params).json()
-		column_name = column['name'] + ' ' + tasklist[column] + ': ' + str(len(task_data)) # СЧЁТЧИК ЗАДАЧ
-		print(column_name)
-		if not task_data:
-			print('\t' + 'Нет задач!')
-			continue
-		for task in task_data:
-			print('\t' + task['name'])
+    column_data = requests.get(base_url.format('boards') + '/' + board_id + '/lists', params=auth_params).json()
+    tasklist = [] # СПИСОК, В КОТОРЫЙ БУДЕМ КЛАСТЬ ID ТАСКОВ
+    for column in column_data:
+        task_data = requests.get(base_url.format('lists') + '/' + column['id'] + '/cards', params=auth_params).json()
+        for task in task_data:
+            tasklist.append(task["id"])
+        column_name = column['name'] + ' ' + tasklist[-1] + ': ' + str(len(task_data)) # СЧЁТЧИК ЗАДАЧ
+        print(column_name)
+        if not task_data:
+            print('\t' + 'Нет задач!')
+            continue
+        for task in task_data:
+            print('\t' + task['name'])
 def create(name, column_name):
-	column_data = requests.get(base_url.format('boards') + '/' + board_id + '/lists', params=auth_params).json()
-	for column in column_data:
-		if column['name'] == column_name:
-			requests.post(base_url.format("cards"), data={'name': name, 'idList': column['id'], **auth_params})
-			break
+    column_data = requests.get(base_url.format('boards') + '/' + board_id + '/lists', params=auth_params).json()
+    for column in column_data:
+        if column['name'] == column_name:
+            requests.post(base_url.format("cards"), data={'name': name, 'idList': column['id'], **auth_params})
+            break
 def colreate(column_name): # ФУНКЦИЯ СОЗДАЕТ НОВУЮ КОЛОНКУ, ПРИНИМАЕТ НАЗВАНИЕ КОЛОНКИ (COLUMN + CREATE = COLREATE)
-	column_data = requests.get(base_url.format('boards') + '/' + board_id + '/lists', params=auth_params).json()
-	for column in column_data:
-		if column['name'] != column_name:
-			requests.post(base_url.format('lists'), data={'name': column_name, 'idBoard': column['idBoard'], **auth_params})
-			break
-		else:
-			print("A column with the same name already exists. Column was not created.")
-			break
+    column_data = requests.get(base_url.format('boards') + '/' + board_id + '/lists', params=auth_params).json()
+    for column in column_data:
+        if column['name'] != column_name:
+            requests.post(base_url.format('lists'), data={'name': column_name, 'idBoard': column['idBoard'], **auth_params})
+            break
+        else:
+            print("A column with the same name already exists. Column was not created.")
+            break
 def move(name, column_name):
-	tasklist = [] # СПИСОК, В КОТОРЫЙ БУДЕМ КЛАСТЬ ID ТАСКОВ
-	collist = [] # СПИСОК, В КОТОРЫЙ БУДЕМ КЛАСТЬ NAME КОЛОНОК
-	column_data = requests.get(base_url.format('boards') + '/' + board_id + '/lists', params=auth_params).json()
-	for column in column_data:
-		column_tasks = requests.get(base_url.format('lists') + '/' + column['id'] + '/cards', params=auth_params).json()
-		for task in column_tasks:
-			if task['name'] == name:
-				tasklist.append(task["id"])
-				collist.append(column["name"])
-	for column in column_data:
-		if column['name'] == column_name:
-			print("Задача 1 находится в колонке '{0}', её id={1}; Задача 2 находится в колонке '{2}', её id={3}".format(collist[0], tasklist[0], collist[1], tasklist[1]))
-			a = int(input("Какое значение вам необходимо переместить? (1 или 2)")) # ФУНКЦИЯ НЕ РАБОТАЕТ НА 3 И БОЛЕЕ ЭЛЕМЕНТАХ (ПО УСЛОВИЮ ЗАДАНИЯ)
-			if a == 1:
-				requests.put(base_url.format('cards') + '/' + tasklist[0] + '/idList', data={'value': column['id'], **auth_params})
-				break
-			elif a == 2:
-				requests.put(base_url.format('cards') + '/' + tasklist[1] + '/idList', data={'value': column['id'], **auth_params})
-				break
-			else:
-				break
+    tasklist = [] # СПИСОК, В КОТОРЫЙ БУДЕМ КЛАСТЬ ID ТАСКОВ
+    collist = [] # СПИСОК, В КОТОРЫЙ БУДЕМ КЛАСТЬ NAME КОЛОНОК
+    column_data = requests.get(base_url.format('boards') + '/' + board_id + '/lists', params=auth_params).json()
+    for column in column_data:
+        column_tasks = requests.get(base_url.format('lists') + '/' + column['id'] + '/cards', params=auth_params).json()
+        for task in column_tasks:
+            if task['name'] == name:
+                tasklist.append(task["id"])
+                collist.append(column["name"])
+    for column in column_data:
+        if column['name'] == column_name:
+            print("Задача 1 находится в колонке '{0}', её id={1}; Задача 2 находится в колонке '{2}', её id={3}".format(collist[0], tasklist[0], collist[1], tasklist[1]))
+            a = int(input("Какое значение вам необходимо переместить? (1 или 2)")) # ФУНКЦИЯ НЕ РАБОТАЕТ НА 3 И БОЛЕЕ ЭЛЕМЕНТАХ (ПО УСЛОВИЮ ЗАДАНИЯ)
+            if a == 1:
+                requests.put(base_url.format('cards') + '/' + tasklist[0] + '/idList', data={'value': column['id'], **auth_params})
+                break
+            elif a == 2:
+                requests.put(base_url.format('cards') + '/' + tasklist[1] + '/idList', data={'value': column['id'], **auth_params})
+                break
+            else:
+                break
 if __name__ == "__main__":
-	# <-- СЮДА МОЖНО ВСТАВЛЯТЬ ФУНКЦИИ ДЛЯ ТЕСТОВ. ПРИМЕРЫ ТЕСТОВ: colreate("На будущее"), move("Изучить Python", "В процессе")
-	if len(sys.argv) <= 2:
-		read()
-	elif sys.argv[1] == 'colreate':
-		colreate(sys.argv[2])
-	elif sys.argv[1] == 'create':
-		create(sys.argv[2], sys.argv[3])
-	elif sys.argv[1] == 'move':
-		move(sys.argv[2], sys.argv[3])
+    # <-- СЮДА МОЖНО ВСТАВЛЯТЬ ФУНКЦИИ ДЛЯ ТЕСТОВ. ПРИМЕРЫ ТЕСТОВ: colreate("На будущее"), move("Изучить Python", "В процессе")
+    if len(sys.argv) <= 2:
+        read()
+    elif sys.argv[1] == 'colreate':
+        colreate(sys.argv[2])
+    elif sys.argv[1] == 'create':
+        create(sys.argv[2], sys.argv[3])
+    elif sys.argv[1] == 'move':
+        move(sys.argv[2], sys.argv[3])
 # СПАСИБО ЗА ПРОВЕРКУ
